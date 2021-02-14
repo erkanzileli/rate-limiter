d433346ab5683aa39104140a63cb7b7fa0b7f3b0
Erkan Zileli <erkanzileli@gmail.com>
2021-02-14 19:05:35 +0300

    limiting now works based on rule patterns instead of requests

--- pkg/service/rate-limit-service/rate_limit_service.go
+++ pkg/service/rate-limit-service/rate_limit_service.go
@@ -3,16 +3,16 @@ package rate_limit_service
 import (
 	"context"
 	"fmt"
+	"github.com/erkanzileli/rate-limiter/pkg/model"
 	"github.com/erkanzileli/rate-limiter/pkg/repository"
 	"github.com/erkanzileli/rate-limiter/pkg/repository/rate-limit-rule-repository"
 	"log"
-	"regexp"
 	"time"
 )
 
 const (
-	requestKeyFormat         = "%s_%s"
-	windowedRequestKeyFormat = "%s_%s_%s"
+	requestHashFormat  = "%s %s"
+	windowedRuleFormat = "%s_%s"
 )
 
 type RateLimitService interface {
@@ -33,29 +33,44 @@ func New(
 // CanProceed takes key and tries to found a pattern that matching with given key.
 // When it found then it compares pattern value with actualUsage.
 func (s *service) CanProceed(ctx context.Context, method, uri string) (bool, error) {
-	windowedRequestKey := fmt.Sprintf(windowedRequestKeyFormat, method, uri, getTimeWindow())
+	requestHash := fmt.Sprintf(requestHashFormat, method, uri)
+	matchedMinimumLimitRule := findMatchedMinimumLimitRule(s.ruleRepository.GetRules(), requestHash)
 
-	actualUsage, err := s.cacheRepository.Increment(ctx, windowedRequestKey)
+	if matchedMinimumLimitRule == nil {
+		return true, nil
+	}
+
+	windowedPatternKey := fmt.Sprintf(windowedRuleFormat, matchedMinimumLimitRule.Pattern, getTimeWindow())
+	actualUsage, err := s.cacheRepository.Increment(ctx, windowedPatternKey)
+
 	if err != nil {
+		log.Printf("Request can't be limited due to cache repository error: %+v\n", err)
+		return true, err
+	}
+
+	if actualUsage > matchedMinimumLimitRule.Limit {
+		log.Printf("Key %s cannot be processed due to pattern %s with limit %d was exceeded and actual is %d\n",
+			requestHash, matchedMinimumLimitRule.Pattern, matchedMinimumLimitRule.Limit, actualUsage)
 		return false, err
 	}
 
-	rules := s.ruleRepository.GetRules()
-	requestKey := fmt.Sprintf(requestKeyFormat, method, uri)
+	return true, nil
+}
 
+// findMatchedMinimumLimitRule loops over whole rules and returns a rule that has matched with the requestHash and its limit is lowest
+func findMatchedMinimumLimitRule(rules []*model.Rule, requestHash string) (matchedMinimumLimitRule *model.Rule) {
 	for _, rule := range rules {
-		if matched, err := regexp.MatchString(rule.Pattern, requestKey); err == nil {
-			if matched && actualUsage > rule.Limit {
-				log.Printf("Key %s cannot be processed due to pattern %s with limit %d was exceeded and actual is %d\n",
-					requestKey, rule.Pattern, rule.Limit, actualUsage)
-				return false, nil
+		if matched := rule.Regex.MatchString(requestHash); matched {
+			if matchedMinimumLimitRule == nil {
+				matchedMinimumLimitRule = rule
+				continue
 			}
-		} else {
-			log.Printf("Error matcing pattern: %s key: %s, err: %+v\n", rule.Pattern, requestKey, err)
+			if rule.Limit < matchedMinimumLimitRule.Limit {
+				matchedMinimumLimitRule = rule
+			}
 		}
 	}
-
-	return true, nil
+	return
 }
 
 // getTimeWindow returns actual time as hhmm format
